944daeb1120eb6262b4d1ba1056af816b551184b
ilanaliouchouche <ilan.aliouchouche@universite-paris-saclay.fr>
2024-05-07 00:22:46 +0200

    pass

--- app/script.js
+++ app/script.js
@@ -1,4 +1,3 @@
 document.getElementById('uploadBtn').addEventListener('click', function() {
     alert('Bouton de chargement cliqué!');
-    // Vous ajouterez ici le code pour gérer le chargement des images et interagir avec les modèles.
 });
