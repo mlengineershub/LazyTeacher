0fef5de6710c3e34ceecca11f480edd54741d5da
ilanaliouchouche <ilan.aliouchouche@universite-paris-saclay.fr>
2024-05-07 00:22:07 +0200

    training of large transformers

---
+++ app/script.js
@@ -0,0 +1,4 @@
+document.getElementById('uploadBtn').addEventListener('click', function() {
+    alert('Bouton de chargement cliqué!');
+    // Vous ajouterez ici le code pour gérer le chargement des images et interagir avec les modèles.
+});
