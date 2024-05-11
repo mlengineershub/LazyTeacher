296621d0815d7999e0313cf02e5139998a7d8294
Ilyes Djerfaf <87201310+idjerfaf@users.noreply.github.com>
2024-05-11 23:35:42 +0200

    Load Image Front -> Transform -> Text || API : Done

--- app/script.js
+++ app/script.js
@@ -1,3 +1,28 @@
 document.getElementById('uploadBtn').addEventListener('click', function() {
     alert('Bouton de chargement cliqué!');
 });
+
+document.getElementById('uploadBtn').addEventListener('click', function () {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const formData = new FormData();
+        formData.append('image', file);
+
+        fetch('http://127.0.0.1:8000/upload/', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                document.getElementById('results').innerText = data.text;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    } else {
+        alert('Please select a file to upload.');
+    }
+});
--- endpoints/endpoint.py
+++ endpoints/endpoint.py
@@ -1,9 +1,17 @@
 # Imports for environment variables
-from fastapi import FastAPI
+import io
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+import pytesseract
+from PIL import Image
 import uvicorn
 
 import os
 from dotenv import load_dotenv
+
+# for windows users
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 # Load environment variables
 ENV_FILE_PATH = (
@@ -18,12 +26,31 @@ app_port = int(os.getenv("PORT_APP"))
 # Initialize the FastAPI app
 app = FastAPI()
 
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=[f"http://{host}:{app_port}"],
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 # Define the endpoints
 @app.get("/")
 async def index():
     return {"message":
             "Hello, please go to /docs to see the API documentation"}
+
+
+@app.post("/upload/")
+async def create_upload_file(image: UploadFile = File(...)):
+    image_data = await image.read()
+    image_pil = Image.open(io.BytesIO(image_data))
+    text = pytesseract.image_to_string(image_pil)
+    return JSONResponse(content={"text": text})
+    return {"message": "Fichier reçu"}
 
 
 if __name__ == "__main__":
